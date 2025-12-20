b16826cdae389b9e40c0681f780780a406ba9603
Shrishti <125468465+shrishti0308@users.noreply.github.com>
2025-12-20 19:33:02 +0530

    [SH] [Add] implement problem routes and validation schemas

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import authRouter from './auth.route';
 import userRouter from './user.route';
+import problemRouter from './problem.route';
 
 const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/users', userRouter);
+router.use('/problems', problemRouter);
 
 export default router;
